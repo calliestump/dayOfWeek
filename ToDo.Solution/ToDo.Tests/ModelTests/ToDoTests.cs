9e868a3eb5b7efde001a5d6f55faed67f2a16c0f
Callie Stump <callie@stu.mp>
2020-12-09 11:39:14 -0500

    inital commit

---
+++ ToDo.Solution/ToDo.Tests/ModelTests/ToDoTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project;
+
+namespace Project.Tests
+{
+  [TestClass]
+  public class ClassTests
+  {
+    [TestMethod]
+     public void Class_Action_Boolean()
+    {
+      //testing code will go here
+    }
+    // Test method will go here
+  }
+}
---
+++ ToDo.Solution/ToDo/Models/ToDo.cs
@@ -0,0 +1,11 @@
+namespace Project
+{
+  public class Class
+  {
+    public bool TODO(int TODO)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
